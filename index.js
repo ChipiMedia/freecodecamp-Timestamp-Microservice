c30c290c53d459c9f0b58d43a71175d0b785af75
Chipi Media <chipi989@outlook.com>
2024-12-24 15:31:49 +0000

    fix: bug fixes

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use((req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`App is running and listening on port ${PORT}`);
 });
 
